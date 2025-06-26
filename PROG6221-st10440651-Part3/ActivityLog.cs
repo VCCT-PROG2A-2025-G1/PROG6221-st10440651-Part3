b4e2eb0e0e5898a95afde61c8ec612115d2cb0c2
ash <st10440651@vcconnect.edu.za>
2025-06-26 17:59:35 +0200

    Added ActivityLog.cs class to track user actions with timestamps for the activity log feature

---
+++ PROG6221-st10440651-Part3/ActivityLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_st10440651_Part3
+{
+    public class ActivityLog
+    {
+        public string Action { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
