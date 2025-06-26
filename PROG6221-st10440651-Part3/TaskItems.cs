0b5410bcabcc3fa36d2629eae0ca856e60437254
ash <st10440651@vcconnect.edu.za>
2025-06-26 17:47:07 +0200

    Added TaskItem.cs class to store task details (title, description, reminder) for task management feature

---
+++ PROG6221-st10440651-Part3/TaskItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PROG6221_st10440651_Part3
+{
+    public class TaskItems
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Reminder { get; set; }
+    }
+}
