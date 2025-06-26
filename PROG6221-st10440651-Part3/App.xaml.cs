92af7e0db6dc40d533f1c700b7ab3042af1a5000
ash <st10440651@vcconnect.edu.za>
2025-06-26 16:58:20 +0200

    Add project files.

---
+++ PROG6221-st10440651-Part3/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PROG6221_st10440651_Part3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
